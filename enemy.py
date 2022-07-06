7f17c2916776d411060dfd57a857da2de55599bc
Pawel <deflorennno@gmail.com>
2022-07-06 23:51:45 +0200

    Stworzenie klasy Enemy, inicjalizacja pierwszego wroga, testowanie

---
+++ enemy.py
@@ -0,0 +1,14 @@
+import pygame
+
+import settings_file
+
+
+class EnemyBlueGhost:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        blue_ghost_img = pygame.image.load("assets/enemy/enemy_1.png").convert_alpha()
+        self.image = pygame.transform.scale(blue_ghost_img, (400, 400)).convert_alpha()
+
+    def draw(self, window):
+        window.blit(self.image, (self.x, self.y))
--- main.py
+++ main.py
@@ -3,24 +3,26 @@ import world_file
 from settings_file import *
 import menu_file
 import button
+import enemy
 
 # Inicjalizacja gry
 pygame.init()
 
-
+'''
 running_game = False
 menu_init = menu_file.Menu()
 button_start = button.Button(0, 500, start_button_img, 0.8)
 button_quit = button.Button(60, 700, exit_button_img, 0.025)
 menu_init.draw()
-
-
 '''
+
 # Główna linia gry
 
 world = world_file.World(world_data)
 
 player = player_file.Player(100, SCREEN_HEIGHT-600)
+
+enemy_1 = enemy.EnemyBlueGhost(700, SCREEN_HEIGHT-600)
 
 running_game = True
 while running_game:
@@ -28,6 +30,7 @@ while running_game:
     world.draw()
     player.update()
     player.draw()
+    enemy_1.draw(screen)
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -39,4 +42,3 @@ while running_game:
 
 pygame.quit()
 
-'''
